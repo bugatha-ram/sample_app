0f84c3ef8ced095ff0e54d65182c19b7025ea5e0
bugatha-rb <bugatha.rb@gmail.com>
2017-04-16 19:19:38 +0000

    Complete advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# Below two lines of code for showing test results green and red
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
